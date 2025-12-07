d9370dfa5d2161ca3801641c44c48624b3a42c28
maicongaldino <maicongaldino460@gmail.com>
2025-12-07 19:30:43 -0300

    :sparkles: feat: adiciona models Aluno, Endereco e Contato

---
+++ src/models/aluno_models.py
@@ -0,0 +1,22 @@
+from dataclasses import dataclass
+from typing import Optional
+
+@dataclass
+class Endereco:
+    rua: str
+    numero: str
+    bairro: str
+    cidade: str
+    uf: str
+
+@dataclass
+class Contato:
+    telefone: str
+    email: str
+
+@dataclass
+class Aluno:
+    matricula: str
+    nome: str
+    endereco: Endereco
+    contato: Contato
